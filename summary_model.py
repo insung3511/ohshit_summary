46585f8d9107cb8993b504b3bbe2c86e97d01863
Restyled.io <commits@restyled.io>
2023-02-16 06:38:17 +0000

    Restyled by black

--- summary_model.py
+++ summary_model.py
@@ -6,17 +6,21 @@ SUMARIZE_MAX_LEN = 130
 SUMARIZE_MIN_LEN = 30
 SUMARIZE_SAMPLE = False
 
-TEST_TEXT_PATH = 'test_text.txt'
+TEST_TEXT_PATH = "test_text.txt"
 MODEL_MODE = "summarization"
 MODEL_ARTC = "facebook/bart-large-cnn"
 
 summarizer = pipeline(MODEL_MODE, MODEL_ARTC)
 
-with open(TEST_TEXT_PATH, 'r') as f:
-    lines = f.read().split('\n')
+with open(TEST_TEXT_PATH, "r") as f:
+    lines = f.read().split("\n")
 
 # Summarize part
 print(
-    summarizer(lines, max_length=SUMARIZE_MAX_LEN,
-               min_length=SUMARIZE_MIN_LEN, do_sample=SUMARIZE_SAMPLE)
+    summarizer(
+        lines,
+        max_length=SUMARIZE_MAX_LEN,
+        min_length=SUMARIZE_MIN_LEN,
+        do_sample=SUMARIZE_SAMPLE,
+    )
 )
