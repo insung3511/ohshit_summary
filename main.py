f7f82ffd25d057ee7222580ca6bb4815618b8b52
insung3511 <insung.park123@gmail.com>
2023-02-17 14:47:20 +0900

    PDF File convert to text.

---
+++ main.py
@@ -0,0 +1,8 @@
+from pdf_to_text import PDF2Text
+
+FILE_PATH = 'test_pdf.pdf'
+saveAsFile = True
+saveFilePath = 'converted_file.txt'
+
+converter = PDF2Text(FILE_PATH=FILE_PATH, saveAsFile=saveAsFile, saveFilePath=saveFilePath)
+converter.converter()
