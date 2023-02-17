74fda82c82a1a4ec5d3eace51dd0da999bf3aacd
insung3511 <insung.park123@gmail.com>
2023-02-17 13:56:58 +0900

    Chrome extension project

--- summary_model.py
+++ summary_model.py
@@ -1,25 +1,27 @@
 import os
-
 from transformers import pipeline
 
-SUMARIZE_MAX_LEN = 130
-SUMARIZE_MIN_LEN = 30
-SUMARIZE_SAMPLE = False
+class SummaryModel():
+    def __init__(self, SUMARIZE_MAX_LEN, SUMARIZE_MIN_LEN, SUMARIZE_SAMPLE, TEST_TEXT_PATH, MODEL_MODE, MODEL_ARTC):
+        self.SUMARIZE_MAX_LEN = SUMARIZE_MAX_LEN # 130
+        self.SUMARIZE_MIN_LEN = SUMARIZE_MIN_LEN # 30
+        self.SUMARIZE_SAMPLE  = SUMARIZE_SAMPLE  # False
 
-TEST_TEXT_PATH = "test_text.txt"
-MODEL_MODE = "summarization"
-MODEL_ARTC = "facebook/bart-large-cnn"
+        self.TEST_TEXT_PATH = TEST_TEXT_PATH     # test_text.txt
+        self.MODEL_MODE     = MODEL_MODE         # "summarization"
+        self.MODEL_ARTC     = MODEL_ARTC         # "facebook/bart-large-cnn"
 
-summarizer = pipeline(MODEL_MODE, MODEL_ARTC)
+    def Model(self):
+        summarizer = pipeline(self.MODEL_MODE, self.MODEL_ARTC)
 
-with open(TEST_TEXT_PATH, "r") as f:
-    lines = f.read().split("\n")
+        with open(self.TEST_TEXT_PATH, "r") as f:
+            lines = f.read().split("\n")
 
-# Summarize part
-print(
-    summarizer(
-        lines,
-        max_length=SUMARIZE_MAX_LEN,
-        min_length=SUMARIZE_MIN_LEN,
-        do_sample=SUMARIZE_SAMPLE,
-    ))
+        # Summarize part
+        print(
+            summarizer(
+                lines,
+                max_length=self.SUMARIZE_MAX_LEN,
+                min_length=self.SUMARIZE_MIN_LEN,
+                do_sample=self.SUMARIZE_SAMPLE,
+            ))
